1f7cdd57b040149a796138dca31e0bdf2addc1ad
brendon cecilio <brendon.cecilio@gmail.com>
2020-09-25 14:46:08 -0400

    project start

---
+++ AnimatedGradient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimatedGradient
+//
+//  Created by Brendon Cecilio on 9/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
